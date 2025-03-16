48c1e62272607ed998c6c81bfe98ca6ab59d1ae8
deividinacio <deivid_@outlook.com>
2025-03-16 16:56:03 -0300

    Primeiro commit: Conexão com Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "projacademico-51345", appId: "1:834596079083:web:8a00386c81e0c7dda0c5ae", storageBucket: "projacademico-51345.firebasestorage.app", apiKey: "AIzaSyCjdHiwAI2p0uN3DNtoFM73CB7vhmi_7-Q", authDomain: "projacademico-51345.firebaseapp.com", messagingSenderId: "834596079083" })), provideFirestore(() => getFirestore())]
 };
